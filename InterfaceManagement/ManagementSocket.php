6c12b3d37355b1ef72ce996cf2eb957effa63384
Felix Hutnyk <felix.hutnyk@gmail.com>
2021-01-09 01:33:29 +0000

    fixes

--- InterfaceManagement/ManagementSocket.php
+++ InterfaceManagement/ManagementSocket.php
@@ -9,7 +9,7 @@ class ManagementSocket {
     public function open($socketAddress, $timeOut = 5)  {
         $socket = \stream_socket_client($socketAddress, $errno, $errstr, $timeOut);
         if (false === $socket) {
-            throw new ManagementSocketException(
+            throw new SocketException(
                 \sprintf('%s (%d)', $errstr, $errno)
             );
         }
@@ -20,7 +20,7 @@ class ManagementSocket {
 
     public function command($command) {
         if (null === $this->socket) {
-            throw new ManagementSocketException('socket not open');
+            throw new SocketException('socket not open');
         }
         self::write($this->socket, \sprintf("%s\n", $command));
 
@@ -29,16 +29,16 @@ class ManagementSocket {
 
     public function close() {
         if (null === $this->socket) {
-            throw new ManagementSocketException('socket not open');
+            throw new SocketException('socket not open');
         }
         if (false === \fclose($this->socket)) {
-            throw new ManagementSocketException('unable to close the socket');
+            throw new SocketException('unable to close the socket');
         }
     }
 
     private static function write($socket, $data) {
         if (false === \fwrite($socket, $data)) {
-            throw new ManagementSocketException('unable to write to socket');
+            throw new SocketException('unable to write to socket');
         }
     }
 
@@ -47,7 +47,7 @@ class ManagementSocket {
         while (!\feof($socket) && !self::isEndOfResponse(\end($dataBuffer))) {
             $readData = \fgets($socket, 4096);
             if (false === $readData) {
-                throw new ManagementSocketException('unable to read from socket');
+                throw new SocketException('unable to read from socket');
             }
             $dataBuffer[] = \trim($readData);
         }
